57d98fac3d4f4dd5838efe2efd85d0e081fb3dab
cuongnv219 <cuongnv219@gmail.com>
2018-12-22 09:23:56 +0700

    Add inline gson

--- base/src/main/java/com/utils/ext/Extensions.kt
+++ base/src/main/java/com/utils/ext/Extensions.kt
@@ -3,7 +3,11 @@ package com.utils.ext
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
 import com.jakewharton.rxbinding2.view.RxView
+import com.utils.ListOfSomething
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import java.util.concurrent.TimeUnit
@@ -14,6 +18,24 @@ import java.util.concurrent.TimeUnit
 fun ViewGroup.inflateExt(layoutId: Int) = LayoutInflater.from(context).inflate(layoutId, this, false)
 
 fun View.clickWithDebounce(debounceTime: Long = 600L, action: (view: View) -> Unit): Disposable =
-        RxView.clicks(this)
-                .throttleFirst(debounceTime, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread())
-                .subscribe { action(this) }
+    RxView.clicks(this)
+        .throttleFirst(debounceTime, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread())
+        .subscribe { action(this) }
+
+inline fun <reified T> Gson.toList(json: String) =
+    this.fromJson<List<T>>(
+        json,
+        ListOfSomething<T>(T::class.java)
+    )
+
+inline fun <reified T> Gson.toList(jsonArr: JsonArray) =
+    this.fromJson<List<T>>(
+        jsonArr,
+        ListOfSomething<T>(T::class.java)
+    )
+
+inline fun <reified T> Gson.toList(jsonObject: JsonObject) =
+    this.fromJson<List<T>>(
+        jsonObject,
+        ListOfSomething<T>(T::class.java)
+    )
