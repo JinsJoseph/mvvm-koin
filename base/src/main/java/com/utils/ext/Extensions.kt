205796b96502661eb0e8b6b15f1b4ad3e2635016
cuongnv219 <cuongnv219@gmail.com>
2019-01-08 11:33:54 +0700

    Update textView extensions

--- base/src/main/java/com/utils/ext/Extensions.kt
+++ base/src/main/java/com/utils/ext/Extensions.kt
@@ -1,8 +1,11 @@
 package com.utils.ext
 
+import android.os.Build
+import android.support.annotation.DrawableRes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.google.gson.Gson
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
@@ -56,4 +59,28 @@ fun View.isVisible() = this.visibility == View.VISIBLE
 
 fun View.setVisibility(isVisible: Boolean) {
     this.visibility = if (isVisible) View.VISIBLE else View.GONE
+}
+
+fun TextView.setDrawableStart(@DrawableRes start: Int) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        this.setCompoundDrawablesRelativeWithIntrinsicBounds(start, 0, 0, 0)
+    }
+}
+
+fun TextView.setDrawableTop(@DrawableRes top: Int) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        this.setCompoundDrawablesRelativeWithIntrinsicBounds(0, top, 0, 0)
+    }
+}
+
+fun TextView.setDrawableEnd(@DrawableRes end: Int) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        this.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, end, 0)
+    }
+}
+
+fun TextView.setDrawableBottom(@DrawableRes bottom: Int) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        this.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, 0, bottom)
+    }
 }
